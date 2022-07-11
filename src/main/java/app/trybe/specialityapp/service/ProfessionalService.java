a6f86c7692a469bfd1e54eba0fd944ed762458c2
Humberto-Bonadiman <humberto_bonadiman@hotmail.com>
2022-07-11 10:43:22 -0400

    create ProfessionalService

--- src/main/java/app/trybe/specialityapp/service/ProfessionalService.java
+++ src/main/java/app/trybe/specialityapp/service/ProfessionalService.java
@@ -1,5 +1,38 @@
 package app.trybe.specialityapp.service;
 
+import app.trybe.specialityapp.model.Professional;
+import app.trybe.specialityapp.repository.ProfessionalRepository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProfessionalService {
 
+  @Autowired
+  private ProfessionalRepository repository;
+
+  public Professional addProfessional(Professional professional) {
+    return repository.save(professional);
+  }
+
+  public List<Professional> professionalList() {
+    return repository.findAll();
+  }
+
+  /**
+   * update professional by id.
+   */
+  public Professional updateProfessional(Professional professional, Integer id) {
+    Professional getProfessional = repository.findById(id).get();
+    getProfessional.setName(professional.getName());
+    getProfessional.setSpeciality(professional.getSpeciality());
+    return repository.save(getProfessional);
+  }
+
+  public void deleteProfessionalById(Integer id) {
+    repository.deleteById(id);
+  }
 }
