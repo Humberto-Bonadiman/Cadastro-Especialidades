2398d4ef365c5487604756b21116a0df3d54a60e
Humberto-Bonadiman <humberto_bonadiman@hotmail.com>
2022-07-10 23:36:58 -0400

    create class Professional

---
+++ src/main/java/app/trybe/specialityapp/model/Professional.java
@@ -0,0 +1,56 @@
+package app.trybe.specialityapp.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Professional {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Integer id;
+
+  @Column
+  private String name;
+
+  @Column
+  private String speciality;
+
+  public Professional() {}
+
+  /**
+   * COnstructor Professional.
+   */
+  public Professional(Integer id, String name, String speciality) {
+    this.id = id;
+    this.name = name;
+    this.speciality = speciality;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getSpeciality() {
+    return speciality;
+  }
+
+  public void setSpeciality(String speciality) {
+    this.speciality = speciality;
+  }
+}
