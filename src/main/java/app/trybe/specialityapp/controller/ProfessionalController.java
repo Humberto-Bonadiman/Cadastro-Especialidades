aa61fe59b1ba4d93eca75434991f29c43ada50fe
Humberto-Bonadiman <humberto_bonadiman@hotmail.com>
2022-07-11 11:30:05 -0400

    create findAll in controller

--- src/main/java/app/trybe/specialityapp/controller/ProfessionalController.java
+++ src/main/java/app/trybe/specialityapp/controller/ProfessionalController.java
@@ -1,11 +1,37 @@
 package app.trybe.specialityapp.controller;
 
+import app.trybe.specialityapp.commons.ApplicationError;
+import app.trybe.specialityapp.model.Professional;
+import app.trybe.specialityapp.service.ProfessionalService;
+import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 @Path("/professional")
 public class ProfessionalController {
+  @Autowired
+  private ProfessionalService service;
 
+  /**
+   * find all.
+   */
+  @GET
+  @Path("/all")
+  @Consumes("application/json")
+  @Produces("application/json")
+  public Response findAll() {
+    List<Professional> professionalList = service.professionalList();
+    if (professionalList.isEmpty()) {
+      return Response.status(Response.Status.NOT_FOUND).entity(
+          new ApplicationError(Response.Status.NOT_FOUND, "Nenhum registro foi encontrado!"))
+          .build();
+    }
+    return Response.status(Response.Status.OK).entity(professionalList).build();
+  }
 }
